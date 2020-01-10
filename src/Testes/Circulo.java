a1121c2cb67dd133a73305b16f8918d915fa5fe8
Merciana <merciana.goncalves@gmail.com>
2020-01-10 13:40:11 -0300

    correcoes

--- src/Testes/Circulo.java
+++ src/Testes/Circulo.java
@@ -2,19 +2,22 @@ package Testes;
 
 public class Circulo {
     private double raio;
-    private static final double PI = 3,14;
+    private static final double PI = 3.14;
 
-    public circulo( double raio){
+    public Circulo( double raio) {
         this.raio = raio;
     }
 
     public double calculaArea(){
+
         return PI*raio*raio;
     }
     public double calcularPerimetro(){
+
         return 2*PI*raio;
     }
     public boolean ehMaior(final Circulo c){
+
         return this.raio > c.raio;
     }
 }
--- src/Testes/Main.java
+++ src/Testes/Main.java
@@ -1,25 +1,27 @@
-package br.ufrn.tads.poo;
+package Testes;
 
 import Testes.Circulo;
 import Testes.Retangulo;
+
 
 public class Main {
     public static void main(String[] args) {
         //maneira correta! de calcular este retangulo e não um retangulo qualquer
         // System.out.println("Hello world!");
         Retangulo r = new Retangulo(2, 4);
-        int area = r.calcula_a_area();
         System.out.println("Area de r:" + r.calcula_a_area());
-
-        r = new Retangulo(3, 4);
-        System.out.println("Area de r:" + r.calcula_a_area());
-
-        System.out.println(Circulo.PI);
 
         Integer a = null;
         Integer b = 3;
         System.out.println(a+b);
-        }
 
-    }
+        r = new Retangulo(3, 4);
+        System.out.println("Area de r:" + r.calcula_a_area());
+        }
 }
+
+
+        /*Humano Merciana = new Humano();
+        Merciana.nome= "Merciana";
+        System.out.println(Merciana.nome);
+         */
